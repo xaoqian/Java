b8bb0cacbae49e0c20bd977df2b340ac9d908d04
haoqian <33185351+xaoqian@users.noreply.github.com>
2018-03-01 21:28:46 +0300

    Add files via upload

--- pack_1/WordStat/WordStatIndex.java
+++ pack_1/WordStat/WordStatIndex.java
@@ -1,29 +1,65 @@
-package com.WordStatIndex
+package com.readfile;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import javax.swing.*;
+import javax.swing.event.*;
 import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import static java.lang.System.in;
+import static java.lang.System.out;
 
 public class WordStatIndex{
-    public static void main(String [] args) throws IOException {
-        String source = null;
-        String key = ""
-        /* дабавить текст*/
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        key = "as";
-        int num = GetFrequency(source, key);
-        System.out.println(key + "встречаемости слов" + num);
-    }
-    public static int GetFrequncecy(String source,String key){
-        int i,j,count = 0;
-        int len1 = source.length();/*длина этого текста*/
-        int len2 = key.length();/*длина,которую еще не подсчитал*/
-        for (i = 0;i < len1 - len2;i++){
-            for (j = 0; j < len2; j++){/*сравнить слово с выражением*/
-                if (key.charAt(j) != scource.charAt(j + i)){
-                    break;
+    public static void main(String[] args){
+        Scanner input = new Scanner(in);
+        String path = input.next();
+        int countword = 0;
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(path));
+        File file = new File("");//введите адрес файла
+        StringBuffer contents = new StringBuffer();
+        contents.append).append(System.getProperty("Line.separator"));
+        String test = text.toLowerCase();
+        StringTokenizer text = new StringTokenizer(", -");
+        while (br.read() != -1) {
+            String s = br.readLine();
+            countChar += s.length();
+            countword += s.split("");
+            isr.close();
+            out,println("char count"+ countChar);
+            out.println("word count"+ countword);
+        }
+        public static void main(String[] args)
+        {
+            try {
+                FileReader fr = new FileReader("");
+                String input = null;
+                while((input = in.read.Line()) != null) {
+                    String str[] = input.split(";");
+                    out.write(str[1]);
+                    out.newLine();
+                    out.flush();
+                }
+
+            }
+            catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+
+        }
+        finally {
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
                 }
             }
-            if ( j>= key.length() ){
-                count++;
-            }
-            return count;
-        }
-    }
+
+
+
+
